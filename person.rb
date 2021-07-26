e90fd81126ee811dba9a9ce7a49207c7bd986bd6
Michael Alex Lynch <mlynch5187@gmail.com>
2021-07-26 13:13:28 -0600

    Create person class

---
+++ person.rb
@@ -0,0 +1,7 @@
+class Person
+  attr_reader :name, :cash
+  def initialize(name, cash)
+    @name = name
+    @cash = cash
+  end
+end
--- person_test.rb
+++ person_test.rb
@@ -3,10 +3,12 @@ require "./person"
 
 class PersonTest < Minitest::Test
   def setup
-    @minerva = Person.new
+    @minerva = Person.new("Minerva", 1000)
+    @luna = Person.new("Luna", 500)
   end
 
   def test_it_exists
     assert_instance_of Person, @minerva
+    assert_instance_of Person, @luna
   end
 end
